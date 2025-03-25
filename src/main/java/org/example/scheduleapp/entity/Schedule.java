f980cf9e6d37ea618075417c7ddcafb9505ce2cd
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-03-25 09:26:20 +0900

    feat : error 수정

--- src/main/java/org/example/scheduleapp/entity/Schedule.java
+++ src/main/java/org/example/scheduleapp/entity/Schedule.java
@@ -1,10 +1,12 @@
 package org.example.scheduleapp.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Schedule {
 
